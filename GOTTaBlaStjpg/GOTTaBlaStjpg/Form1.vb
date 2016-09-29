4f34b76e6a300470f4b9560754cd75444e2a0f32
bb118314 <roflwhy@gmail.com>
2016-09-29 11:13:15 -0500

    Add files via upload

---
+++ GOTTaBlaStjpg/GOTTaBlaStjpg/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim intCount As Integer
+
+        For intCount = -100 To 300
+            PictureBox1.Left = intCount
+        Next
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+    End Sub
+End Class
